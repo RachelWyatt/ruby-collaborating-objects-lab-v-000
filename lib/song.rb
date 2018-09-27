447a8a21848f3531590a99cff952df4b5da49f00
Rachel Wyatt <rw126@evansville.edu>
2018-09-27 20:00:14 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,8 +4,13 @@ class Song
 
   def initialize(name)
     @name = name
+    @artist = nil
   end
 
+  def artist=(artist)
+    @artist = artist
+    artist.add_song(self) 
+  end
 
   def self.new_by_filename(file_name)
   end
