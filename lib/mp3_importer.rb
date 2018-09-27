ac30e52c3e7e14f313e37ba11afa09a1613fecfc
Rachel Wyatt <rw126@evansville.edu>
2018-09-27 19:51:18 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,12 +1,16 @@
 class MP3Importer
+  attr_accessor :path
 
   def initialize(file_path)
+    @path = path
   end
 
   def files
+    Dir.entries(path).reject {|f| if f == '.' || f == '- -'}
   end
 
   def import
+    files.each {|f| Song.new_by_filename(f)}
   end
 
 end
